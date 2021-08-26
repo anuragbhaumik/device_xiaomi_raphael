0d4a9f8cdba72ace9f6f2d388a3a7753c094c7ab
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-26 02:48:37 +0200

    raphael: rootdir: Initial libperfmgr init config
    * From coral, heavily stripped down
    
    Change-Id: Ib6846e4f4f23baf56910d2330e26846b99830218

--- device.mk
+++ device.mk
@@ -249,6 +249,7 @@ PRODUCT_PACKAGES += \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
