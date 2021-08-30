7dd6662e7db8537b8e1d9b70c0ac214d9365d915
anuragbhaumik <bhaumik463@gmail.com>
2021-08-30 06:37:10 +0000

    raphael: nuke more prebuilt apps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps Photos Youtube YoutubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps Youtube YoutubeMusicPrebuilt PrebuiltGmail Drive Maps AmbientSensePrebuilt AppDirectedSMSService ConnMO DCMO MyVerizonServices VZWAPNLib VzwOmaTrigger obdm_stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
